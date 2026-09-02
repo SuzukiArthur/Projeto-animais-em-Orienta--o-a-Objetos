d063b8f6d748d828f373a2db702e06dff196b51b
Felipe de Jesus <felipesouzadejesustd2@gmail.com>
2026-09-02 16:48:55 -0300

    Update Mamifero.java

--- ProjetoAnimais/Mamifero.java
+++ ProjetoAnimais/Mamifero.java
@@ -1,1 +1,18 @@
 package ProjetoAnimais;
+
+public class Mamifero {
+
+   
+    private String cor_pelo;
+    private char domestico;
+    
+    
+
+    public Mamifero(String cor_pelo, char domestico) {
+       
+        this.cor_pelo = cor_pelo;
+        this.domestico = domestico;
+      
+    }
+
+}
