bd57bfbd285edaf1bbda48259614e5adfabcbe9d
Arthur Yuji Mendes Suzuki <yujisaccont@gmail.com>
2026-09-02 16:45:50 -0300

    construtor de animal

--- ProjetoAnimais/Animal.java
+++ ProjetoAnimais/Animal.java
@@ -1,1 +1,15 @@
 package ProjetoAnimais;
+
+public class Animal{
+    private String nome;
+    private double peso;
+    private String especie;
+    private int idade;
+
+    public Animal(){
+        this.nome = "";
+        this.peso = 0.0;
+        this.especie = "";
+        this.idade = 0;
+    }
+}
